4eb16884c9027ff2d284448fab772a4378972cbb
Tairo Nomura <tairo_nomura@mac.com>
2020-10-15 07:00:42 +0900

    Add STEMDuArchade features

---
+++ hardware/STEMDu/avr/libraries/STEM_Du/STEMDuArchade.h
@@ -0,0 +1,25 @@
+//
+// STEMDuArchade.h
+//
+// STEM Du Archade related class, function
+//
+// Update:Oct 13, 2020 1:13
+//
+// 1.0 2020101201 : Initial file
+//
+
+#ifndef STEM_Du_Archade_h
+#define STEM_Du_Archade_h
+
+#include "bitmap.h"
+
+class Sprite {
+    public:
+    int x;
+    int y;
+    bool isShow;
+    Sprite(int x, int y, )
+
+}
+
+#endif
